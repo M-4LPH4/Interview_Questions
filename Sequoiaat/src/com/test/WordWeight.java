ce85f4da9027695ca2a2e9a26533a9e720ee79e9
Prakash VL <prakashkkml@gmail.com>
2018-05-03 15:12:10 +0530

    update WordWeight.java
    removed comments and scanner closing moved, according to the need.

--- Sequoiaat/src/com/test/WordWeight.java
+++ Sequoiaat/src/com/test/WordWeight.java
@@ -9,12 +9,11 @@ public class WordWeight {
 	public static void main(String[] args) {
 		System.out.println("Input: A word");
 		Scanner sc= new Scanner(System.in);
-		sc.close();
 		String wordWeight = sc.nextLine();
+		sc.close();
 		wordWeight = wordWeight.toUpperCase();
 		Integer totalWeight = 0;
 		for (char character : wordWeight.toCharArray()) {
-			System.out.println(((int)character -_64));
 			totalWeight += ((int)character -_64);
 		}
 		System.out.println("Weight of the input word: "+totalWeight);
